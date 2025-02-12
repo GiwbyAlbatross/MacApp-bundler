8eebca8ddd27e308e93230211521eb5ea5796988
GiwbyAlbatross <giwby.albatross@gmail.com>
2025-02-12 15:09:35 +1100

    Mostly finished finction `pybundler.entrypoint.getentrypoint`, remains untested but I'll address that issue later.

--- pybundler/entrypoint.py
+++ pybundler/entrypoint.py
@@ -1,18 +1,35 @@
 from typing import Union
 import os, pathlib
 
-def getentrypoint(cwd: typing.Union[str, bytes, os.PathLike],
+pathish = Union[str, os.PathLike]
+
+def getentrypoint(cwd: Union[str, bytes, os.PathLike],
+                  assetfolder: pathish=None,
+                  executable: pathish=...,
                   flags: Union[tuple[str], list[str]]=tuple()) -> str:
-    "Write entrypoint code to put in App.app/Contents/MacOS/"
-    if isinstance(cwd, bytes):
-        path = cwd.decode()
-    else:
-        path = str(cwd)
+    """Write entrypoint code to put in App.app/Contents/MacOS/
+    cwd: cwd relative to assetfolder
+    assetfolder: name of assetfolder (not used?)
+    executable: path of executable, relative to assetfolder
+    flags: list of flags to pass to python interpreter"""
+
     basic = """#!/usr/bin/env python3
 from sys import argv, flags, executable
 from pathlib import Path
 from os import getcwd, chdir, system
+import os.path as path
 
 path = Path(__file__).absolute()
-os.chdir(path.parents[1])"""
-    #
+os.chdir(str(path.parents[2]))
+os.chdir(path.join('Resources', {assetfolder}))
+assetfolder = Path(os.getcwd())
+os.chdir('{cwd}')
+
+executable = assetfolder / '{executable}'
+
+system(executable + ' '.join({flags}) + str(executable))
+"""
+    return basic.format(cwd=str(cwd),
+                        assetfolder=str(assetfolder),
+                        executable=str(executable),
+                        flags=repr(flags))
